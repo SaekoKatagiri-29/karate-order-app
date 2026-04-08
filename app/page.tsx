e2dc5f40d749b996288227fe52397e4e71e1909f
Saeko Katagiri <pndpnd1616@gmail.com>
2026-04-08 23:25:48 +0900

    fix: force dynamic for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from '@/lib/prisma'
 import Link from 'next/link'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const [teamCount, playerCount, osakaPlayerCount, matchCount] = await Promise.all([
     prisma.team.count(),
